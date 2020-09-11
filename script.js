d3431232e04e1b515c3c555073d0c8b063040c29
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-10 22:23:16 -0700

    Created JQuery elements

--- script.js
+++ script.js
@@ -5,3 +5,47 @@ currentDay = today;
 console.log(today);
 
 
+
+
+
+/*
+<section class="row">
+    <div class="col-md-4">
+    5:00am
+    </div>
+    <div class="col-md-5">
+        <input type="text" class="input" id="5am">
+        </div>
+    <div class="col-md-3">
+        <button class="save-btn" date-time='5am' id="save">Save</button>
+        </div>
+</section>
+*/
+
+let container = $('.container')
+let time;
+time = '5:00am' 
+let newRow = $('<section>').addClass('row');
+let hourCell = $('<div>').addClass('col-md-4');
+hourCell.text(time);
+newRow.append(hourCell);
+
+let inputCell = $('<div>').addClass('col-md-5');
+let input = $('<input type = "text">');
+input.addClass('input');
+input.attr('id', time);
+inputCell.append(input);
+newRow.append(inputCell);
+
+let saveBtnCell = $('<div>').addClass('col-md-3');
+let saveBtn = $('<button>').addClass('save-btn');
+saveBtn.attr('date-time', time);
+saveBtn.text('save');
+saveBtnCell.append(saveBtn);
+newRow.append(saveBtnCell);
+container.append(newRow);
+
+
+
+
+
