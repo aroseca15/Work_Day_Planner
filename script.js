7bf1315afbc8b338eab37e0e0cc3938350f95efa
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-15 08:36:46 -0700

    Text area set completely.

--- script.js
+++ script.js
@@ -13,43 +13,29 @@ $('#currentDay').text(todaysDate.toLocaleString('default', {month:'long'})+" "+t
 $('#currentTime').text(todaysDate.toLocaleString('en-US', {hour: 'numeric',minute:'numeric', hour12: true}))
 
 
-
-/*
-<section class="row">
-    <div class="col-md-4">
-    5:00am
-    </div>
-    <div class="col-md-5">
-        <input type="text" class="input" id="5am">
-        </div>
-    <div class="col-md-3">
-        <button class="save-btn" date-time='5am' id="save">Save</button>
-        </div>
-</section>
-*/
-
-let container = $('.container');
-let time;
-// Make repeatable   
+const container = $('.container');
+// Making each row repeatable   
 
 let times = ['5:00am', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm','3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm'];
-// let time;
+let time;
 let index = 0;
 while (index < times.length) {
 
     let time = times[index];
     let newRow = $('<section>').addClass('row');
-    let hourCell = $('<div>').addClass('col-md-4');
+    let hourCell = $('<div>').addClass('col-2');
     hourCell.text(time);
     newRow.append(hourCell);
 
-    let inputCell = $('<div>').addClass('col-md-5');
-    let input = $('<input type = "text">');
-    input.addClass('input');
-    input.attr('id', time);
-    inputCell.append(input);
-    newRow.append(inputCell);
-    let saveBtnCell = $('<div>').addClass('col-md-3');
+    let textAreaCell = $('<div>').addClass('col-8').addClass('textarea');
+    let textArea = $('<textarea>');
+    textArea.attr('cols', '85')
+    textArea.addClass('time-block');
+    textArea.attr('id', time);
+    textAreaCell.append(textArea);
+    newRow.append(textAreaCell);
+    
+    let saveBtnCell = $('<div>').addClass('col-2');
     let saveBtn = $('<button>').addClass('save-btn');
     saveBtn.attr('date-time', time);
     saveBtn.text('save');
@@ -59,7 +45,37 @@ while (index < times.length) {
     index++;
 }
 
-$('.col-md-4').css('background-color: #ffffff', 'color: #000000', 'border-top: 1px dashed #000000');
+// I am super close. But not quite there!!
+$('.save-btn').on('click', function(){
+    let value = $(this).siblings('.textarea').val();
+    let time = $(this).parent().attr('date-time');
+    localStorage.setItem(time, value);
+    localStorage.getItem(time, value);
+})
+
+
+// // For below see if there is a way to set your past, cuurent, future as:
+// moment().isBefore(Moment|String|Number|Date|Array); //false or true   moment().isBefore() for comparing to #currentTime. If true then space is grey.
+// moment().isSame(Moment|String|Number|Date|Array); //false or true     moment().isSame() for comparing to #currentTime. If true then space is red.
+// moment().isAfter(Moment|String|Number|Date|Array); //false or true    moment().isAfter() for comparing to #currentTime. If true then space is green. 
+// //Maybe something like this:
+// if (moment('time').isSame('#currentTime')) {
+//     display red
+// } else if (moment('time').isBefore('#currentTime')) {
+//     disply grey
+// }else if (moment('time').isAfter('#currentTime')) {
+//     display green
+// };
+
+
+
+
+
+
+
+
+// $('.col-md-4').css('background-color: #ffffff', 'color: #000000', 'border-top: 1px dashed #000000');
+
 
 
 
